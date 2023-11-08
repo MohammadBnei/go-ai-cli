960c9303fccb1620555d6474003322a941217f11
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-11-08 22:28:13 +0100

    fix(build): added mock speech cmd to permit build of app without portaudio

---
+++ cmd/speech/speechMock.go
@@ -0,0 +1,20 @@
+//go:build !portaudio
+
+package speech
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"moul.io/banner"
+)
+
+var SpeechCmd = &cobra.Command{
+	Use:   "speech",
+	Short: "Convert your speech into text.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(banner.Inline("go ai cli - speech"))
+
+		fmt.Println("Speech is not supported on this platform")
+	},
+}
